11d91939e899b7be0609bcaf5c0a24b809991879
Rei Tanaka <91103379+vraintanakarei@users.noreply.github.com>
2025-05-15 17:39:32 +0900

    use pyproject.toml

--- setup.py
+++ setup.py
@@ -295,56 +295,12 @@ ext_modules = [
 ]
 
 ################################################################################
-# Packages
+# Extensions
 ################################################################################
 
-# no need to do fancy stuff so far
-packages = setuptools.find_packages()
-
-install_requires.extend([
-    'onnx'
-])
-
-################################################################################
-# Test
-################################################################################
-
-setup_requires.append('pytest-runner')
-
-if sys.version_info[0] == 3:
-    # Mypy doesn't work with Python 2
-    extras_require['mypy'] = ['mypy==0.600']
-
-################################################################################
-# Final
-################################################################################
-
-# read the contents of your README file
-from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
+# No need to specify packages and other details explicitly here
+# as they're now defined in pyproject.toml
 setuptools.setup(
-    name="onnxoptimizer",
-    version=VersionInfo.version,
-    description="Open Neural Network Exchange",
     ext_modules=ext_modules,
     cmdclass=cmdclass,
-    packages=packages,
-    license='Apache License v2.0',
-    include_package_data=True,
-    install_requires=install_requires,
-    setup_requires=setup_requires,
-    extras_require=extras_require,
-    author='ONNX Optimizer Authors',
-    author_email='onnx-technical-discuss@lists.lfai.foundation',
-    url='https://github.com/onnx/optimizer',
-    keywords='deep-learning ONNX',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    entry_points={
-        'console_scripts': [
-            'onnxoptimizer=onnxoptimizer:main',
-        ],
-    },
 )
